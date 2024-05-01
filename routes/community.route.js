bc7942411da72838f6c818c2f88bb56440c856f6
Waqas Akram <waqasassasin@gmail.com>
2024-05-02 00:28:25 +0500

    Remove admin auth from getCommunityDetail api

--- routes/community.route.js
+++ routes/community.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.route("/getAll").get(protect, getCommunities);
 router.route("/register").post(protect, admin, registerCommunity);
 router.route("/addSlots").post(protect, admin, addSlots);
-router.route("/:id").get(protect, admin, getCommunityDetail)
+router.route("/:id").get(protect, getCommunityDetail)
 
 export default router;
