de34049b306b8777ecccd46fe0a9ac442b6bcbcf
Waqas Akram <waqasassasin@gmail.com>
2024-06-05 00:40:55 +0500

    Add check in user profile api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -245,6 +245,9 @@ const socialAuth = asyncHandler(async (req, res) => {
 // @route   GET /api/v1/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
+  if(!req.user){
+    errorResponse(res, "Token is required", statusCodes.NOT_FOUND);
+  }
   const user = await User.findById(req.user._id);
 
   if (user) {
